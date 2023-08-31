4ba23dfb7a5b1c254dc8a8c1893af639813594b2
TITUS KILUNDI <muthiani42@gmail.com>
2023-08-31 12:50:31 +0300

    version 2

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const wrapperContainer = document.createElement("div");
 wrapperContainer.setAttribute("class", "wrapperContainer");
 mainContainer.appendChild(wrapperContainer);
 
+// Create day container
+const dayDiv = document.createElement("div");
+dayDiv.setAttribute("class", "dayDiv");
+wrapperContainer.appendChild(dayDiv);
+
 // Create time content container
 const timeContent = document.createElement("div");
 timeContent.setAttribute("class", "timeContent");
@@ -40,22 +45,62 @@ const seconds = document.createElement("span");
 seconds.setAttribute("id", "sec");
 timeContent.appendChild(seconds);
 
-// Update time every second using setInterval
-setInterval(() => {
-  let date = new Date();
+// Create ampm element
+const ampm = document.createElement("span");
+ampm.setAttribute("class", "ampm");
+timeContent.appendChild(ampm);
 
-  // Get hours and format it
+// Create yearDiv element
+const yearDiv = document.createElement("div");
+yearDiv.setAttribute("class", "yearDiv");
+wrapperContainer.appendChild(yearDiv);
+
+// Update time every second using setInterval
+const setInter = setInterval(() => {
+  let date = new Date();
+  const dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const dayName = dayNames[date.getDay()];
+  dayDiv.innerHTML = `${dayName}`;
+
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+  const formattedDate = `${month}/${day}/${year}`;
+  yearDiv.innerHTML = `${formattedDate}`;
+
   let hour =
     date.getHours() < 10 ? "0" + date.getHours() : "" + date.getHours();
   hours.innerHTML = hour;
 
-  // Get minutes and format it
   let minute =
     date.getMinutes() < 10 ? "0" + date.getMinutes() : "" + date.getMinutes();
   minutes.innerHTML = minute;
 
-  // Get seconds and format it
   let second =
     date.getSeconds() < 10 ? "0" + date.getSeconds() : "" + date.getSeconds();
   seconds.innerHTML = second;
+
+  let period = "AM"; // Default to AM
+  if (hour >= 12) {
+    period = "PM";
+    if (hour > 12) {
+      hour -= 12;
+    }
+  } else if (hour === 0) {
+    hour = 12;
+  }
+  hour = hour < 10 ? "0" + hour : "" + hour;
+  hours.innerHTML = `${hour}`;
+  ampm.innerText = period;
 }, 1000);
+
+// clearInterval(setInter);
